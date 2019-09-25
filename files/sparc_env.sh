a1b9832be521cbef13b368a09d6ac15c7326bb1b
Ben Comer <ben.comer@gatech.edu>
2019-09-25 12:06:20 -0400

    Update sparc_env.sh

--- files/sparc_env.sh
+++ files/sparc_env.sh
@@ -5,15 +5,15 @@ module load mvapich2/2.1
 module load git
 module load anaconda3/4.2.0
 
-export PATH=/gpfs/pace1/project/chbe-medford/medford-share/builds/sparc/dev_new_inputs/SPARC/lib/:$PATH
-export PYTHONPATH=/gpfs/pace1/project/chbe-medford/medford-share/gits/frozen/ase_3.18.0b1/ase:$PATH
-export PATH=/gpfs/pace1/project/chbe-medford/medford-share/gits/frozen/ase_3.18.0b1/ase/bin:$PATH
-export PYTHONPATH=/gpfs/pace1/project/chbe-medford/medford-share/gits/dev/ase-sparc/rewrite/pysparcx/:$PYTHONPATH
-export SPARC_PSP_PATH=/gpfs/pace1/project/chbe-medford/medford-share/data/pseudos/oncv_2015/PBE_psp_cut
+export PATH=/nv/pace-ice/bcomer3/SPARC/SPARC/lib:$PATH
+export PYTHONPATH=/nv/pace-ice/bcomer3/ase/ase-ase-3.18.1:$PYTHONPATH
+export PATH=/nv/pace-ice/bcomer3/ase/ase-ase-3.18.1/bin:$PATH
+export PYTHONPATH=/nv/pace-ice/bcomer3/sparc-ase/pysparcx/:$PYTHONPATH
+export SPARC_PSP_PATH=/nv/pace-ice/bcomer3/sparc_psps/PBE_psp_cut
 
 
 if [[ -z "${PBS_NP}" ]]; then
-  export ASE_SPARC_COMMAND="/gpfs/pace1/project/chbe-medford/medford-share/builds/sparc/dev_new_inputs/SPARC/lib/sparc -name PREFIX > log"
+  export ASE_SPARC_COMMAND="/nv/pace-ice/bcomer3/SPARC/SPARC/lib/sparc -name PREFIX > log"
 else
-  export ASE_SPARC_COMMAND="mpirun -np $PBS_NP /gpfs/pace1/project/chbe-medford/medford-share/builds/sparc/dev_new_inputs/SPARC/lib/sparc -name PREFIX > log"
+  export ASE_SPARC_COMMAND="mpirun -np $PBS_NP /nv/pace-ice/bcomer3/SPARC/SPARC/lib/sparc -name PREFIX > log"
 fi
